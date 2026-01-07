94ebfe9bdfb8da9d0bf0109098695a3996295738
sashakw1376 <s.watanabe@ufl.edu>
2026-01-07 11:20:05 -0500

    hawk-eye setup scripts

--- injects/hawk-eye/hawk-eye.sh
+++ injects/hawk-eye/hawk-eye.sh
@@ -1,1 +1,3 @@
-docker run -it --rm -v /home/results:/app/results -v /home/blue/injects/hawk-eye/connection.yml:/app/connection.yml -v /home/blue/injects/hawk-eye/fingerprint.yml:/app/fingerprint.yml --add-host=host.docker.internal:host-gateway --connection /app/connection.yml --fingerprint /app/fingerprint.yml
+#!/bin/bash
+
+docker run -it --rm -v /home/blue/injects/hawk-eye/results:/app/results -v /home/blue/injects/hawk-eye/connection.yml:/app/connection.yml -v /home/blue/injects/hawk-eye/fingerprint.yml:/app/fingerprint.yml --add-host=host.docker.internal:host-gateway --connection /app/connection.yml --fingerprint /app/fingerprint.yml
